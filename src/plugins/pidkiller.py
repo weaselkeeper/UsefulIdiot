f1fbe1283674b099b0a134a4c853f1848f7fceb4
David Wahlstrom <david.wahlstrom@memorylane.com>
2013-01-09 13:11:50 -0800

    do not kill pid 1, because that is just a reboot

--- src/plugins/pidkiller.py
+++ src/plugins/pidkiller.py
@@ -3,6 +3,8 @@
 def run(options={}):
     import os
     from random import choice
-    pids = [pid for pid in os.listdir('/proc') if pid.isdigit() and pid > 1]
+    pids = pid for pid in os.listdir('/proc') if pid.isdigit()
+    if 1 in pids:
+        pids.remove(1)
     print 'I would have killed: ' + choice(pids)
 
