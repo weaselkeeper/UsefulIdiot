94f826b5df155720f61b9aa6cbdcd7dcc516092e
Jim Richardson <weaselkeeper@gmail.com>
2013-01-09 12:57:03 -0800

    exclude pid 1 from death sentance

--- src/plugins/pidkiller.py
+++ src/plugins/pidkiller.py
@@ -3,6 +3,6 @@
 def run(options={}):
     import os
     from random import choice
-    pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+    pids = [pid for pid in os.listdir('/proc') if pid.isdigit() and pid > 1]
     print 'I would have killed: ' + choice(pids)
 
