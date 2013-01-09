83f4a98a264a2b79e95c8ccdaa3d8e0974cf3597
David Wahlstrom <david.wahlstrom@memorylane.com>
2013-01-09 08:12:05 -0800

    add gitignore files to ignore compiled python files update usefulidiot.conf to include a plugin_dir option to allow the user to specify where to load plugins from prelim support for dynamic plugin loading/executing for UsefulIdiot.py

--- src/UsefulIdiot.py
+++ src/UsefulIdiot.py
@@ -26,9 +26,13 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 :email: david.wahlstrom@gmail.com
 
 """
+# used in UsefulIdiot()
 import os
 import sys
 import logging
+
+#used in ConfigFile()
+import ConfigParser
 
 logging.basicConfig(level=logging.WARN,
                     format='%(asctime)s %(levelname)s - %(message)s',
@@ -44,23 +48,34 @@ class UsefulIdiot(object):
     """Object to instantiate and control a useful idiot"""
     log.debug('in class UsefulIdiot()')
 
-    def __init__(self, plugin):
+    def __init__(self, plugin, path):
         """Initialize the idiot"""
-        log.debug('in UsefulIdiot().__init__(self, %s)' % plugin)
+        log.debug('in UsefulIdiot().__init__(self, %s, %s)' % (plugin, path))
+
+        import imp
+
+        log.debug('importing ' + path)
+        self.module = imp.load_source(plugin, path)
+
+    def run(self):
+        """Execute the plugin's run method"""
+        log.debug('in UsefulIdiot().run()')
+
+        self.module.run()
 
 class ConfigFile(object):
     """Object to facilitate config file access"""
     log.debug('in class ConfigFile()')
 
-    import ConfigParser
     filename = None
     configparser = None
 
     def __init__(self, filename):
         """Initialize ConfigFile object"""
         log.debug('in ConfigFile().init(self, %s)' % filename)
+
         self.filename = filename
-        self.configparser = self._get_config(filename)
+        self.configparser = self.get_config(filename)
 
     def get_config(self, myfile):
         """Load config file for parsing"""
@@ -76,15 +91,16 @@ class ConfigFile(object):
             section, hard_fail))
 
         def do_fail(err):
-           if hard_fail:
-              log.error(err)
-              sys.exit(-1)
-           else:
-              log.info(err)
+            log.debug('in ConfigFile().get_item().do_fail(%s)' % err)
+            if hard_fail:
+                log.error(err)
+                sys.exit(-1)
+            else:
+                log.info(err)
 
         item = None
         try:
-           item = self.configparser.get(section, cfgitem)
+            item = self.configparser.get(section, cfgitem)
         except ConfigParser.NoOptionError, e:
             do_fail(e)
         except ConfigParser.NoSectionError, e:
@@ -98,18 +114,23 @@ if __name__ == "__main__":
 
     import argparse
     from random import choice
+
     cmd_parser = argparse.ArgumentParser(
         description='Command a useful idiot to do something to your server')
     cmd_parser.add_argument('-d', '--debug', dest='debug',
         action='store_true', help='Enable debugging during execution',
         default=None)
+    args = cmd_parser.parse_args()
+
+    if args.debug:
+        log.setLevel(logging.DEBUG)
 
     configfile = '/etc/usefulidiot/usefulidiot.conf'
     if os.path.isfile(configfile):
         log.debug('reading config from: %s' % configfile)
-    elif os.path.isfile('../usefulidiot.conf'):
-        log.debug('reading config from: ../conf/usefulidiot.conf')
-        configfile = '../conf/usefulidiot.conf'
+    elif os.path.isfile('../config/usefulidiot.conf'):
+        log.debug('reading config from: ../config/usefulidiot.conf')
+        configfile = '../config/usefulidiot.conf'
     else:
         log.debug('unable to read config file: %s' % configfile)
         sys.exit(1)
@@ -117,8 +138,32 @@ if __name__ == "__main__":
 
     plugins = []
     plugins.append(cfg.get_item('plugins'))
+    if not plugins:
+        log.debug('no plugins found')
+        print 'No plugins found. Exiting.'
+        sys.exit(1)
     log.debug('found plugin(s): %s' % plugins)
     loaded_plugin = choice(plugins)
     log.debug('randomly selected plugin: %s' % loaded_plugin)
 
-    idiot = UsefulIdiot(loaded_plugin)
+    log.debug('locating plugin to load...')
+    plugin_dir = cfg.get_item('plugin_dir')
+    if not os.path.isdir(plugin_dir):
+        log.debug('plugin directory does not exist: %s' % plugin_dir)
+        print 'Plugin directory does not exist: %s' % plugin_dir
+        sys.exit(1)
+    log.debug('attempting to load plugin from %s' % plugin_dir)
+    if os.path.isfile(plugin_dir + loaded_plugin):
+        log.debug('plugin found at: ' + plugin_dir + loaded_plugin)
+        plugin_path = plugin_dir + loaded_plugin
+        loaded_plugin = loaded_plugin.split('.')[0]
+    elif os.path.isfile(plugin_dir + loaded_plugin + '.py'):
+        log.debug('plugin found at: ' + plugin_dir + loaded_plugin + '.py')
+        plugin_path = plugin_dir + loaded_plugin + '.py'
+    else:
+        log.debug('unable to find plugin')
+        print 'Unable to locate plugin ' + loaded_plugin
+        sys.exit(1)
+
+    idiot = UsefulIdiot(loaded_plugin, plugin_path)
+    idiot.run()
---
+++ src/plugins/pidkiller.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+def run():
+    print "it worked!"
+
