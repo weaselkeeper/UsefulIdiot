e89c6d9ac7f93f2eace584c3ddbd05f2298d6e13
Jim Richardson <weaselkeeper@gmail.com>
2013-03-18 21:43:09 -0700

    Determine correct command location and options for service check by distro.
    service command location and options vary by distro, as well as by OS. For now, focusing on Linux, we peel off by distro

--- src/plugins/bounce_service.py
+++ src/plugins/bounce_service.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
+# Bare bones start, only works for RH and friends, due to location of service
+# comamand
+# TODO  Need to be a bit more catholic in detecting service status.
+
+def oscheck():
+    import platform
+    if platform.dist()[0] == 'debian':
+        check_service = '/usr/sbin/service'
+        check_service_opts = '--status-all'
+    else:
+        check_service = '/sbin/service'
+        check_service_opts = '--list-all'
+
+    return check_service, check_service_opts
+
 def run(options={}):
     import subprocess
     from random import choice
-    servicelist = subprocess.check_output(['/sbin/service','--status-all'])
+    _service,_opts=oscheck()
+    servicelist = subprocess.check_output([_service,_opts])
     print servicelist
 
 run()
