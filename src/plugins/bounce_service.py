9ebf3a88de89f62066acb4fb738c291a5b6662a1
Jim Richardson <weaselkeeper@gmail.com>
2013-03-18 22:29:44 -0700

    Basic work on the bounce service module, it has some drawbacks, since some daemons don't handle status requests well

--- src/plugins/bounce_service.py
+++ src/plugins/bounce_service.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python
 
-# Bare bones start, only works for RH and friends, due to location of service
-# comamand
-# TODO  Need to be a bit more catholic in detecting service status.
 
-def oscheck():
-    import platform
-    if platform.dist()[0] == 'debian':
-        check_service = '/usr/sbin/service'
-        check_service_opts = '--status-all'
-    else:
-        check_service = '/sbin/service'
-        check_service_opts = '--list-all'
+# This only works on sysv init systems for now. 
+# Look at the contents of /etc/init.d, pick a random one, and see if it's
+# currently running, if it is not, drop it from the list, and make another
+# random pick, repeat until you get a running service. Then restart it, and
+# return the results to the calling process.
+# This is a dangerous module as some daemons don't take kindly to status
+# requests,  we might want to reconsider this one.
 
-    return check_service, check_service_opts
+# for now, all this does, is find a running service to play with, but as noted,
+# there are dangers.
 
-def run(options={}):
-    import subprocess
-    from random import choice
-    _service,_opts=oscheck()
-    servicelist = subprocess.check_output([_service,_opts])
-    print servicelist
+import os
+import subprocess
+from random import shuffle
 
-run()
+
+def select_service():
+    not_running = 1
+    s_list = os.listdir('/etc/init.d')
+    while not_running:
+        shuffle(s_list)
+        service = '/etc/init.d/' + s_list.pop()
+        try:
+            service_status = subprocess.Popen([service,'status'],
+                stdout=subprocess.PIPE).communicate()[0]
+        except:
+            OSError
+            # try again
+            break
+        if 'running' in service_status.lower():
+            not_running = 0
+    return service,service_status
+
+
+service,status = select_service()
+print service,status
