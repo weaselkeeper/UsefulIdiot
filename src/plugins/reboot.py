910478955afcc94c245ec97be252b29499582537
Jim Richardson <weaselkeeper@gmail.com>
2013-06-26 10:52:25 -0700

    further work on reboot plugin, adding some error handling, and better info during dry run

--- src/plugins/reboot.py
+++ src/plugins/reboot.py
@@ -13,24 +13,30 @@ import subprocess
 def run(options={}):
     """main loop for this plugin"""
 
-    from random import choice
-
     success = 0
     message = 'reboot unsuccessful'
 
     if 'dryrun' in options:
         if options['dryrun'] == True:
-            message = 'I would have rebooted this server'
+            import socket
+            hostname = socket.gethostname()
+            message = 'I would have rebooted server %s' % hostname
             return success, message
-
-    reboot_system()
-
-    return success, message
+    success,message = reboot_system()
+    return success,message
 
 def reboot_system():
     """you realize this is dangerous, right?"""
-    shutdown = 'echo "shutting down"'
     #shutdown = "/sbin/shutdown -r now"
-    process = subprocess.Popen(shutdown.split(), stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    print output
+    # Use the following command for testing.
+    shutdown = '/bin/echo "help me!"'
+    try:
+        subprocess.check_call(shutdown.split())
+        success = 1
+        message = 'rebooting server'
+        return success,message
+    except:
+        success = 0
+        message = 'unable to reboot server'
+        return success,message
+run()
